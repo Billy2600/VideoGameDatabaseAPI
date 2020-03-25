ee21087af7de7efa7d8ffe33d5136022c31786a3
Billy <billymcfly@gmail.com>
2020-03-25 11:32:55 -0600

    Add unit testing project

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VideoGameAPI.Repositories;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
